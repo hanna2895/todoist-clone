0248b37eb4ede6cd0696b4554696ce306429a1e2
Hannah Werman <hanna.w28@gmail.com>
2019-09-08 09:20:08 -0500

    adding context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Header } from './components/layout/Header'
 import { Content } from './components/layout/Content';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
 import './App.scss';
 
-export const App = () => (
-  <div className="App">
-    <Header />
-    <Content />
-  </div>
-);
+export const App = () => {
+
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  )
+};
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,41 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
     FaChevronDown, 
     FaInbox, 
     FaRegCalendarAlt, 
     FaRegCalendar
 } from 'react-icons/fa'
+import { useSelectedProjectValue } from '../../context';
 
-export const Sidebar = () => <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-        <li data-testid="inbox" className="inbox">
-            <span>
-                <FaInbox/>
-            </span>
-            <span>Inbox</span>
-        </li>
-        <li data-testid="today" className="today">
-            <span>
-                <FaRegCalendar />
-            </span>
-            <span>Today</span>
-        </li>
-        <li data-testid="next_7" className="next_7">
-            <span>
-                <FaRegCalendarAlt />
-            </span>
-            <span>Next 7 Days</span>
-        </li>
-    </ul>
+export const Sidebar = () => {
+    const { setSelectedProject } = useSelectedProjectValue();
+    const [active, setActive] = useState('INBOX');
+    const [showProjects, setShowProjects] = useState(true);
 
-    <div className="sidebar__middle">
-        <span><FaChevronDown /></span>
-        <h2>Projects</h2>
-    </div>
+    return (
+        <div className="sidebar" data-testid="sidebar">
+            <ul className="sidebar__generic">
+                <li data-testid="inbox" className="inbox">
+                    <span>
+                        <FaInbox/>
+                    </span>
+                    <span>Inbox</span>
+                </li>
+                <li data-testid="today" className="today">
+                    <span>
+                        <FaRegCalendar />
+                    </span>
+                    <span>Today</span>
+                </li>
+                <li data-testid="next_7" className="next_7">
+                    <span>
+                        <FaRegCalendarAlt />
+                    </span>
+                    <span>Next 7 Days</span>
+                </li>
+            </ul>
 
-    <ul className="sidebar__projects">
-        Projects will be here
-    </ul>
+            <div className="sidebar__middle">
+                <span><FaChevronDown /></span>
+                <h2>Projects</h2>
+            </div>
 
-    Add Project Component Here
-</div>
+            <ul className="sidebar__projects">
+                Projects will be here
+            </ul>
+
+            Add Project Component Here
+        </div>
+    )
+}
